fb3bb82b854d11b77cb308bd39709fbf641b062e
Oliver Hansen <hiffidrengen@gmail.com>
2024-03-06 16:39:09 +0100

    Updated models
    There are still some troubles with running the training script; so I am still in the process of debugging those.

--- src/models/FITS_COPY.py
+++ src/models/FITS_COPY.py
@@ -10,21 +10,15 @@ class Model(nn.Module):
 
     def __init__(self, configs):
         super(Model, self).__init__()
-        self.seq_len = configs.seq_len
-        self.pred_len = configs.pred_len
+        self.seq_len = configs.input_length
+        self.pred_len = configs.output_length
         self.individual = configs.individual
-        self.channels = configs.enc_in
+        # self.channels = configs.enc_in
 
-        self.dominance_freq=configs.cut_freq # 720/24
+        self.dominance_freq=configs.dominance_freq # 720/24
         self.length_ratio = (self.seq_len + self.pred_len)/self.seq_len
 
-        if self.individual:
-            self.freq_upsampler = nn.ModuleList()
-            for i in range(self.channels):
-                self.freq_upsampler.append(nn.Linear(self.dominance_freq, int(self.dominance_freq*self.length_ratio)).to(torch.cfloat))
-
-        else:
-            self.freq_upsampler = nn.Linear(self.dominance_freq, int(self.dominance_freq*self.length_ratio)).to(torch.cfloat) # complex layer for frequency upcampling]
+        self.freq_upsampler = nn.Linear(self.dominance_freq, int(self.dominance_freq*self.length_ratio)).to(torch.cfloat) # complex layer for frequency upcampling]
         # configs.pred_len=configs.seq_len+configs.pred_len
         # #self.Dlinear=DLinear.Model(configs)
         # configs.pred_len=self.pred_len
@@ -39,23 +33,23 @@ class Model(nn.Module):
         x = x / torch.sqrt(x_var)
 
         low_specx = torch.fft.rfft(x, dim=1)
+
         low_specx[:,self.dominance_freq:]=0 # LPF
         low_specx = low_specx[:,0:self.dominance_freq,:] # LPF
-        # print(low_specx.permute(0,2,1))
         if self.individual:
             low_specxy_ = torch.zeros([low_specx.size(0),int(self.dominance_freq*self.length_ratio),low_specx.size(2)],dtype=low_specx.dtype).to(low_specx.device)
             for i in range(self.channels):
                 low_specxy_[:,:,i]=self.freq_upsampler[i](low_specx[:,:,i].permute(0,1)).permute(0,1)
         else:
-            low_specxy_ = self.freq_upsampler(low_specx.permute(0,2,1)).permute(0,2,1)
-        # print(low_specxy_)
+            low_specxy_ = self.freq_upsampler(low_specx.permute(0,2,1)).permute(0,2,1)        
+        
         low_specxy = torch.zeros([low_specxy_.size(0),int((self.seq_len+self.pred_len)/2+1),low_specxy_.size(2)],dtype=low_specxy_.dtype).to(low_specxy_.device)
-        low_specxy[:,0:low_specxy_.size(1),:]=low_specxy_ # zero padding
+        low_specxy[:,0:low_specxy_.size(1),:] = low_specxy_ # zero padding
+        
         low_xy=torch.fft.irfft(low_specxy, dim=1)
         low_xy=low_xy * self.length_ratio # compemsate the length change
-        # dom_x=x-low_x
         
-        # dom_xy=self.Dlinear(dom_x)
-        # xy=(low_xy+dom_xy) * torch.sqrt(x_var) +x_mean # REVERSE RIN
         xy=(low_xy) * torch.sqrt(x_var) +x_mean
-        return xy, low_xy* torch.sqrt(x_var)
+        print(xy, torch.sqrt(x_var), x_mean)
+        raise NotImplementedError("This model is not implemented yet")
+        return xy#, low_xy* torch.sqrt(x_var)
