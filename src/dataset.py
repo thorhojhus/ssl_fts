0f4119a4f4fdc803215415bc3d268ac4ec78b803
Thor Hojhus <thorhojhus@gmail.com>
2024-03-13 16:01:56 +0100

    dataset modifications

--- src/dataset.py
+++ src/dataset.py
@@ -5,78 +5,166 @@ from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset, DataLoader
 from typing import List
 import torch
+from dataset_ETT_hour import Dataset_ETT_hour
+from augmentations import augmentation
 
 
 class TimeSeriesDataset(Dataset):
-    def __init__(self, data: ndarray, input_length: int = 384, output_length: int = 96):
-        self.data = data
-        self.input_length = input_length
-        self.output_length = output_length
+    def __init__(
+        self,
+        x_data: ndarray,
+        y_data: ndarray,
+    ):
+        self.x_data = x_data
+        self.y_data = y_data
 
     def __len__(self):
-        return len(self.data) - self.input_length - self.output_length + 1
+        return len(self.x_data)
 
     def __getitem__(self, idx):
-        start = idx
-        end = idx + self.input_length
-        x = self.data[start:end]
-        y = self.data[end : end + self.output_length]
-        return (
-            torch.tensor(x, dtype=torch.float32),
-            torch.tensor(y, dtype=torch.float32),
-        )
+        return self.x_data[idx], self.y_data[idx]
 
 
 def load_and_process_data(
     filepath: str,
     target_columns: List[str],
+    seq_len: int = 720,
+    label_len: int = 48,
+    pred_len: int = 96,
+    mode_flag: str = "train",
     all_cols: bool = True,
-    test_size: float = 0.2,
     normalize: bool = True,
+    test_time_train: bool = True,
+    data_size: int = 1,
+    aug_rate: tuple = (0.5,),
+    augment_data: bool = False,
+    aug_method: str = "f_mix",
 ):
     df = pd.read_csv(filepath)
+
+    mode = {"train": 0, "val": 1, "test": 2}[mode_flag]
+
+    if test_time_train:
+        border1s = [0, 12960 - seq_len, 14400]
+        border2s = [12960, 14400, 14400]
+    else:
+        border1s = [0, 8640 - seq_len, 11520 - seq_len]
+        border2s = [8640, 11520, 14400]
+
+    border1, border2 = border1s[mode], border2s[mode]
 
     if all_cols:
         target_columns = df.columns[1:]
 
-    data = df[target_columns].values
+    df = df[target_columns]
 
     if normalize:
         scaler = StandardScaler()
-        data = scaler.fit_transform(data)
+        data = df[border1s[0] : border2s[0]]
+        data = scaler.fit_transform(data.values)
+    else:
+        data = df.values
 
-    train_data, test_data = (
-        data[: -int(len(data) * test_size)],
-        data[-int(len(data) * test_size) :],
+        # train_data, test_data = (
+        #     data[: -int(len(data) * test_size)],
+        #     data[-int(len(data) * test_size) :],
+        # )
+
+    data_x = data[border1:border2]
+    data_y = data[border1:border2]
+
+    x_data, y_data = split_data(
+        data_x, data_y, seq_len, pred_len, label_len, mode, data_size=1
     )
 
-    return train_data, test_data
+    if augment_data:
+        x_data, y_data = augment(x_data, y_data, aug_method, aug_rate)
+
+    return x_data, y_data
+
+
+def split_data(
+    data_x: ndarray,
+    data_y: ndarray,
+    seq_len: int,
+    pred_len: int,
+    label_len: int,
+    mode: int,
+    data_size: int = 1,
+):
+    x_data = []
+    y_data = []
+    data_len = len(data_x) - seq_len - pred_len + 1
+    mask_data_len = int((1 - data_size) * data_len) if data_size < 1 else 0
+    for i in range(len(data_x) - seq_len - pred_len + 1):
+        if (mode == 0 and i >= mask_data_len) or mode != 0:
+            s_begin = i
+            s_end = s_begin + seq_len
+            r_begin = s_end - label_len
+            r_end = r_begin + label_len + pred_len
+            x_data.append(data_x[s_begin:s_end])
+            y_data.append(data_y[r_begin:r_end])
+    return x_data, y_data
+
+
+def augment(x_data: list, y_data: list, aug_method: str, aug_rate: tuple):
+    aug_size = [1 for i in len(x_data)]
+    for i in range(len(x_data)):
+        for _ in range(aug_size[i]):
+            aug = augmentation("dataset")
+            if aug_method == "f_mask":
+                x, y = aug.freq_dropout(x_data[i], y_data[i], dropout_rate=aug_rate)
+            elif aug_method == "f_mix":
+                rand = float(np.random.random(1))
+                i2 = int(rand * len(x_data))
+                x, y = aug.freq_mix(
+                    x_data[i],
+                    y_data[i],
+                    x_data[i2],
+                    y_data[i2],
+                    dropout_rate=aug_rate,
+                )
+            x_data.append(x)
+            y_data.append(y)
+    return x_data, y_data
 
 
 def create_dataloaders(
-    train_data: ndarray,
-    test_data: ndarray,
-    input_length: int = 384,
-    output_length: int = 96,
-    batch_size: int = 64,
+    x_data_train: list,
+    y_data_train: list,
+    x_data_test: list,
+    y_data_test: list,
+    batch_size: int,
 ):
-    train_dataset = TimeSeriesDataset(train_data, input_length, output_length)
-    test_dataset = TimeSeriesDataset(test_data, input_length, output_length)
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-    return train_loader, test_loader
+    train_dataset = TimeSeriesDataset(x_data_train, y_data_train)
+    test_dataset = TimeSeriesDataset(x_data_test, y_data_test)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+    return train_dataloader, test_dataloader
 
 
 def data_setup(args):
-    train_data, test_data = load_and_process_data(
-        args.filepath, args.target_columns, args.all_cols, args.test_size, args.normalize
+    x_data_train, y_data_train = load_and_process_data(
+        filepath=args.filepath,
+        target_columns=args.target_columns,
+        all_cols=args.all_cols,
+        normalize=args.normalize,
+        seq_len=args.seq_len,
+        label_len=args.label_len,
+        pred_len=args.pred_len,
+        mode_flag="train",
     )
+    x_data_test, y_data_test = load_and_process_data(
+        filepath=args.filepath,
+        target_columns=args.target_columns,
+        all_cols=args.all_cols,
+        normalize=args.normalize,
+        seq_len=args.seq_len,
+        label_len=args.label_len,
+        pred_len=args.pred_len,
+        mode_flag="test",
+    )
+
     return create_dataloaders(
-        train_data,
-        test_data,
-        args.input_length,
-        args.output_length,
-        args.batch_size,
+        x_data_train, y_data_train, x_data_test, y_data_test, args.batch_size
     )
--- src/train.py
+++ src/train.py
@@ -5,29 +5,79 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
-def train(model: nn.Module, data_loader: DataLoader, epochs: int = 1000, device="cuda"):
+def train(
+    model: nn.Module,
+    train_loader: DataLoader,
+    test_loader: DataLoader,
+    epochs: int = 1000,
+    device="cuda",
+    pred_len=360,
+    features="M",
+    ft=False,
+):
     model.to(device)
     criterion = nn.MSELoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
 
-    model.train()
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, "min", factor=0.5, threshold=1e-3
+    )
 
-    for epoch in tqdm(range(epochs), desc="Training Progress"):
+    current_lr = optimizer.param_groups[0]["lr"]
+
+    f_dim = -1 if features == "MS" else 0
+
+    print(f"Initial Learning Rate: {current_lr}")
+
+    for epoch in range(epochs):
+        model.train()
         train_loss = []
-        for batch_x, batch_y in data_loader:
-            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-
+        for batch_x, batch_y in train_loader:
             optimizer.zero_grad()
-
-            output = model(batch_x)
+            # batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+            batch_x = batch_x.float().to(device)
+            batch_y = batch_y.float().to(device)[:, -pred_len:, :]
             batch_xy = torch.cat([batch_x, batch_y], dim=1)
 
-            loss = criterion(output, batch_xy)
+            output = model(batch_x)
+
+            if ft:
+                output = output[:, -pred_len:, f_dim:]
+                batch_y = batch_y[:, -pred_len:, f_dim:].to(device)
+                loss = criterion(output, batch_y)
+            else:
+                output = output[:, :, f_dim:]
+                loss = criterion(output, batch_xy)
+            # print(output.shape)
+            # print(batch_y.shape)
+            # print(batch_x.shape)
             train_loss.append(loss.item())
 
             loss.backward()
             optimizer.step()
+            # print("batch", batch_xy.shape)
+            # print("outputs", output.shape)
+        scheduler.step(np.average(train_loss))
 
-        tqdm.write(f"Epoch: {epoch+1} Loss: {np.mean(train_loss)}")
+        new_lr = optimizer.param_groups[0]["lr"]
+        if current_lr != new_lr:
+            print(f"Learning Rate changed to: {new_lr}")
+            current_lr = new_lr
+
+        print(f"Epoch: {epoch+1} Train loss: {np.average(train_loss)}")
+
+    if test_loader:
+        model.eval()
+        with torch.no_grad():
+            test_loss = []
+
+            for batch_x, batch_y in test_loader:
+                batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+                output = model(batch_x)
+                output = output[:, -pred_len:, :]
+                loss = criterion(output, batch_y)
+                test_loss.append(loss.item())
+
+        print(f"Test loss: {np.average(test_loss)}")
 
     return model
