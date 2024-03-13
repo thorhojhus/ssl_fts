874c244a1c6716aa50b8c64b4dab1554420d6034
Oliver Hansen <hiffidrengen@gmail.com>
2024-03-13 13:50:43 +0100

    Add official model back, and add similarity tests

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import torch
+import pytest
 
 def test_normal_versus_normalization():
     a = torch.rand(1, 4)
@@ -11,3 +12,20 @@ def test_normal_versus_normalization():
     normalized_ts_data_2 = torch.nn.functional.normalize(a, dim=1, p=2)
 
     assert not torch.allclose(normalized_ts_data, normalized_ts_data_2)
+
+
+@pytest.mark.parametrize("individual", ['--individual', '--no-individual'])
+def test_equal_param_sizes(argparser, individual):
+    from src.models.FITS import FITS
+    from src.models.copy_fits import Model
+
+    args = argparser.parse_args(['--device', 'cpu', individual])
+
+    model = FITS(args)
+    model_2 = Model(args)
+
+    def count_parameters(model): return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    assert count_parameters(model) == count_parameters(model_2)
+
+    
