ac85316cd7fa45808a582d3f308445c5b75a1b38
Oliver Hansen <hiffidrengen@gmail.com>
2024-03-20 14:11:15 +0100

    Tests that compare the different steps in the model forward passes - they are now asserted to be equal under a seed!

--- tests/model_test.py
+++ tests/model_test.py
@@ -7,11 +7,15 @@ from src.dataset import data_setup
 from src.models.FITS import FITS
 from src.models.copy_fits import Model
 
+
 def test_normal_versus_normalization():
     a = torch.rand(1, 4)
 
     # 1) Normalization of the input tensor:
-    ts_mean, ts_var = torch.mean(a, dim=1, keepdim=True), torch.var(a, dim=1, keepdim=True) + 1e-10
+    ts_mean, ts_var = (
+        torch.mean(a, dim=1, keepdim=True),
+        torch.var(a, dim=1, keepdim=True) + 1e-10,
+    )
     normalized_ts_data = (a - ts_mean) / ts_var
 
     # 2) compare this with the normlization function
@@ -20,18 +24,19 @@ def test_normal_versus_normalization():
     assert not torch.allclose(normalized_ts_data, normalized_ts_data_2)
 
 
-@pytest.mark.parametrize("individual", ['--individual', '--no-individual'])
+@pytest.mark.parametrize("individual", ["--individual", "--no-individual"])
 def test_equal_param_sizes(argparser, individual):
 
-    args = argparser.parse_args(['--device', 'cpu', individual])
+    args = argparser.parse_args(["--device", "cpu", individual])
 
     model = FITS(args)
     model_2 = Model(args)
 
-    def count_parameters(model): return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
     assert count_parameters(model) == count_parameters(model_2)
-    
+
 
 def test_equal_train(FITS, ogFITS, argparser):
     args = argparser.parse_args()
@@ -42,41 +47,69 @@ def test_equal_train(FITS, ogFITS, argparser):
 
     for batch_x, _ in train_loader:
         # Do a forward pass to get the tensors into debug
+
+        batch_x = batch_x.float().to("cpu")
+
+        atol = 1e-4  # 4 significant digits in our data
+
         FITS(batch_x)
         ogFITS(batch_x)
 
         FITStensors = FITS.debug_tensors
         ogFITStensors = ogFITS.debug_tensors
 
-        test_tensor_1 = FITStensors['ts_frequency_data']
-        test_tensor_2 = ogFITStensors['x']
-        test_tensor_2 = torch.fft.rfft(test_tensor_2, dim=1)
-        assert test_tensor_1.equal(test_tensor_2)
+        test_mean_var_1 = FITStensors["normalized_ts_data"]
+        test_mean_var_2 = ogFITS.debug_tensors["x"]
 
-        test_tensor_1 = test_tensor_1[:,0:FITS.cutoff_frequency,:]
+        test_mean_var_1.equal(test_mean_var_2)
 
-        test_tensor_2[:,ogFITS.dominance_freq:]=0 # LPF
-        test_tensor_2 = test_tensor_2[:,0:ogFITS.dominance_freq,:] # LPF
+        test_rfft_1 = torch.fft.rfft(test_mean_var_1, dim=1)
+        test_rfft_1_comp = FITStensors["ts_frequency_data"]
+        assert torch.allclose(test_rfft_1, test_rfft_1_comp, atol=atol)
 
-        assert test_tensor_1.equal(test_tensor_2)
+        test_rfft_2 = torch.fft.rfft(test_mean_var_2, dim=1)
+        # print indices where the two tensors differ:
+        assert torch.allclose(test_rfft_1_comp, test_rfft_2, atol=atol)
 
-        assert torch.allclose(FITStensors['normalized_ts_data'], ogFITStensors['x'])
+        test_lpf_1 = FITStensors["ts_frequency_data_filtered"]
 
-        print(FITStensors['ts_frequency_data_filtered'][8,0:10,0])
-        print(ogFITStensors['low_specx'][8,0:10,0])
-        for i in range(0, 64):
-            assert torch.allclose(FITStensors['ts_frequency_data_filtered'][9,:,:], ogFITStensors['low_specx'][9,:,:])
+        test_rfft_2[:, ogFITS.dominance_freq :] = 0  # LPF
+        test_lpf_2 = test_rfft_2[:, 0 : ogFITS.dominance_freq, :]  # LPF
+
+        assert torch.allclose(test_lpf_1, test_lpf_2, atol=atol)
+
+        test_complex_data_1 = FITStensors["complex_valued_data"]
+        test_complex_data_2 = ogFITStensors["low_specxy_"]
+
+        assert torch.allclose(test_complex_data_1, test_complex_data_2, atol=atol)
+
+        test_norm_spec_xy_1 = FITStensors["norm_spec_xy"]
+        test_norm_spec_xy_2 = ogFITStensors["low_specxy"]
+
+        assert torch.allclose(test_norm_spec_xy_1, test_norm_spec_xy_2, atol=atol)
+
+        test_norm_xy_1 = FITStensors["norm_xy"]
+        test_norm_xy_2 = ogFITStensors["low_xy"]
+
+        assert torch.allclose(test_norm_xy_1, test_norm_xy_2, atol=atol)
+
+        test_xy_1 = FITStensors["xy"]
+        test_xy_2 = ogFITStensors["xy"]
+
+        assert torch.allclose(test_xy_1, test_xy_2, atol=atol)
+
         break
 
+
 def test_low_pass_equal():
-    a = torch.rand(2,3,2)
+    a = torch.rand(2, 3, 2)
     b = a.clone()
 
     assert a.equal(b)
     assert a is not b
 
     a = a[:, 0:2, :]
-    b[:,2:]=0
+    b[:, 2:] = 0
     b = b[:, 0:2, :]
 
     assert a.equal(b)
