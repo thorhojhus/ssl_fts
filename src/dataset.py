4b144a0cf4a0981f1b3cff520dca6a119be27cfb
Thor Hojhus <thorhojhus@gmail.com>
2024-03-20 16:38:46 +0100

    do train_test split similar to original repo in ett

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
+import os
 from numpy import ndarray
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset, DataLoader
 from typing import List
 import torch
 from .augmentations import augmentation
-from sklearn.model_selection import train_test_split
 
 
 class TimeSeriesDataset(Dataset):
@@ -26,8 +26,9 @@ class TimeSeriesDataset(Dataset):
 
 
 def load_and_process_data(
-    filepath: str,
-    target_columns: List[str],
+    root_path: str = "data",
+    dataset: str = "ETTh1",
+    target_columns: List[str] = "OT",
     seq_len: int = 720,
     label_len: int = 48,
     pred_len: int = 96,
@@ -35,76 +36,77 @@ def load_and_process_data(
     all_cols: bool = True,
     normalize: bool = True,
     test_time_train: bool = False,
-    data_size: int = 1,
     aug_rate: tuple = (0.5,),
     augment_data: bool = False,
     aug_method: str = "f_mix",
 ):
-    df = pd.read_csv(filepath)
-
+    df = pd.read_csv(os.path.join(root_path, str(dataset + ".csv")))
     mode = {"train": 0, "val": 1, "test": 2}[mode_flag]
-
-    # if test_time_train:
-    #     border1s = [0, 12960 - seq_len, 14400]
-    #     border2s = [12960, 14400, 14400]
-    # else:
-    #     border1s = [0, 8640 - seq_len, 11520 - seq_len]
-    #     border2s = [8640, 11520, 14400]
-
-    # border1, border2 = border1s[mode], border2s[mode]
-
     if all_cols:
         target_columns = df.columns[1:]
-
     df = df[target_columns]
-
     if normalize:
         scaler = StandardScaler()
-        # data = df[border1s[0] : border2s[0]]
         data = scaler.fit_transform(df.values)
     else:
         data = df.values
-
-        # train_data, test_data = (
-        #     data[: -int(len(data) * test_size)],
-        #     data[-int(len(data) * test_size) :],
-        # )
-
-    # data_x = data[border1:border2]
-    # data_y = data[border1:border2]
-    data_x = data
-    data_y = data
     x_data, y_data = split_data(
-        data_x, data_y, seq_len, pred_len, label_len, mode, data_size=1
+        data,
+        seq_len,
+        pred_len,
+        label_len,
+        mode,
+        data_size=1,
+        test_time_train=test_time_train,
+        dataset=dataset,
     )
-
     if augment_data:
         x_data, y_data = augment(x_data, y_data, aug_method, aug_rate)
-
     return x_data, y_data
 
 
 def split_data(
-    data_x: ndarray,
-    data_y: ndarray,
+    data: ndarray,
     seq_len: int,
     pred_len: int,
     label_len: int,
     mode: int,
     data_size: int = 1,
+    test_time_train: bool = False,
+    dataset: str = "ETTh1",
 ):
-    x_data = []
-    y_data = []
-    data_len = len(data_x) - seq_len - pred_len + 1
+    if dataset == "ETTh1" or dataset == "ETTh2":
+        border1s = [0, 8640 - seq_len, 11520 - seq_len]
+        border2s = [8640, 11520, 14400]
+        if test_time_train:
+            border1s = [0, 12960 - seq_len, 14400]
+            border2s = [12960, 14400, 14400]
+    elif dataset == "ETTm1" or dataset == "ETTm2":
+        border1s = [0, 34560 - seq_len, 46080 - seq_len]
+        border2s = [34560, 46080, 57600]
+    else:
+        n_train = int(len(data) * 0.8)
+        n_test = int(len(data) * 0.1)
+        n_val = len(data) - n_train - n_test
+        border1s = [0, n_train - seq_len, len(data) - n_test - seq_len]
+        border2s = [n_train, n_train + n_val, len(data)]
+        if test_time_train:
+            border1s = [0, n_train - seq_len, len(data)]
+            border2s = [n_train, len(data), len(data)]
+
+    border1, border2 = border1s[mode], border2s[mode]
+    data = data[border1:border2]
+    data_len = len(data) - seq_len - pred_len + 1
     mask_data_len = int((1 - data_size) * data_len) if data_size < 1 else 0
-    for i in range(len(data_x) - seq_len - pred_len + 1):
+    x_data, y_data = [], []
+    for i in range(len(data) - seq_len - pred_len + 1):
         if (mode == 0 and i >= mask_data_len) or mode != 0:
             s_begin = i
             s_end = s_begin + seq_len
             r_begin = s_end - label_len
             r_end = r_begin + label_len + pred_len
-            x_data.append(data_x[s_begin:s_end])
-            y_data.append(data_y[r_begin:r_end])
+            x_data.append(data[s_begin:s_end])
+            y_data.append(data[r_begin:r_end])
     return x_data, y_data
 
 
@@ -139,14 +141,19 @@ def create_dataloaders(
 ):
     train_dataset = TimeSeriesDataset(x_data_train, y_data_train)
     test_dataset = TimeSeriesDataset(x_data_test, y_data_test)
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, drop_last=True
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, drop_last=False
+    )
     return train_dataloader, test_dataloader
 
 
 def data_setup(args):
-    x_data, y_data = load_and_process_data(
-        filepath=args.filepath,
+    X_train, y_train = load_and_process_data(
+        root_path=args.root_path,
+        dataset=args.dataset,
         target_columns=args.target_columns,
         all_cols=args.all_cols,
         normalize=args.normalize,
@@ -156,6 +163,16 @@ def data_setup(args):
         mode_flag="train",
     )
 
-    X_train, X_test, y_train, y_test = train_test_split(x_data, y_data)
+    X_test, y_test = load_and_process_data(
+        root_path=args.root_path,
+        dataset=args.dataset,
+        target_columns=args.target_columns,
+        all_cols=args.all_cols,
+        normalize=args.normalize,
+        seq_len=args.seq_len,
+        label_len=args.label_len,
+        pred_len=args.pred_len,
+        mode_flag="test",
+    )
 
     return create_dataloaders(X_train, y_train, X_test, y_test, args.batch_size)
