8eea28a35f401133e75228a57f2c84eb62be48c2
Thor Jensen <thorhojhus@gmail.com>
2024-06-11 19:11:33 +0200

    use_original_datahandling should work again

--- run.py
+++ run.py
@@ -7,57 +7,58 @@ from rich import print
 
 warnings.filterwarnings("ignore")
 
-args = parser.parse_args()
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-if args.dominance_freq == 0:
-    args.dominance_freq = int(args.seq_len // args.base_T + 1) * args.H_order + 10
-    print(args.dominance_freq)
+    if args.dominance_freq == 0:
+        args.dominance_freq = int(args.seq_len // args.base_T + 1) * args.H_order + 10
+        print(args.dominance_freq)
 
-if args.use_original_datahandling:
-    # only works on linux for some reason
-    from src.FITS_datahandling.data_factory import data_provider
+    if args.use_original_datahandling:
+        # only works on linux for some reason
+        from src.FITS_datahandling.data_factory import data_provider
 
-    train_data, train_loader = data_provider(args, "train")
-    test_data, test_loader = data_provider(args, "test")
-else:
-    train_loader, test_loader = data_setup(args)
+        train_data, train_loader = data_provider(args, "train")
+        test_data, test_loader = data_provider(args, "test")
+    else:
+        train_loader, test_loader = data_setup(args)
 
-model = FITS(args)
-print(model)
+    model = FITS(args)
+    print(model)
 
-for param in model.parameters():
-    param.data.fill_(0)
+    for param in model.parameters():
+        param.data.fill_(0)
 
-if args.train_and_finetune:
-    model, _ = train(
-        model=model,
-        train_loader=train_loader,
-        test_loader=test_loader,
-        epochs=args.epochs,
-        pred_len=args.pred_len,
-        features=args.features,
-        ft=0,
-        args=args,
-    )
-    model, test_mse = train(
-        model=model,
-        train_loader=train_loader,
-        test_loader=test_loader,
-        epochs=args.epochs,
-        pred_len=args.pred_len,
-        features=args.features,
-        ft=1,
-        args=args,
-    )
+    if args.train_and_finetune:
+        model, _ = train(
+            model=model,
+            train_loader=train_loader,
+            test_loader=test_loader,
+            epochs=args.epochs,
+            pred_len=args.pred_len,
+            features=args.features,
+            ft=0,
+            args=args,
+        )
+        model, test_mse = train(
+            model=model,
+            train_loader=train_loader,
+            test_loader=test_loader,
+            epochs=args.epochs,
+            pred_len=args.pred_len,
+            features=args.features,
+            ft=1,
+            args=args,
+        )
 
-else:
-    model, test_mse = train(
-        model=model,
-        train_loader=train_loader,
-        test_loader=test_loader,
-        epochs=args.epochs,
-        pred_len=args.pred_len,
-        features=args.features,
-        ft=args.ft,
-        args=args,
-    )
+    else:
+        model, test_mse = train(
+            model=model,
+            train_loader=train_loader,
+            test_loader=test_loader,
+            epochs=args.epochs,
+            pred_len=args.pred_len,
+            features=args.features,
+            ft=args.ft,
+            args=args,
+        )
