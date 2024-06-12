edf581d17cb5ffe855a95c5adf45802ca78ff6d6
Thor Hojhus <thorhojhus@gmail.com>
2024-06-12 11:34:04 +0200

    run script and job script

---
+++ job_script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#BSUB -q gpuv100
+#BSUB -J train_fits
+#BSUB -n 8
+#BSUB -R "span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 06:00
+#BSUB -R "rusage[mem=8GB]"
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu-%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
+
+bash /zhome/5c/8/186818/ssl_fts/run_scripts.sh
