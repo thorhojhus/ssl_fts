0daa7f4ffee517f41581a92ea1d9b3e03c74be6e
Andreas <aleefsen@gmail.com>
2024-06-14 10:03:18 +0200

    made some run files for motortemp dataset

--- scripts/motor/motor_720_192.sh
+++ scripts/motor/motor_720_192.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
 python run.py \
     --normalize True \
     --seq_len 720 \
-    --pred_len 336 \
+    --pred_len 192 \
     --dominance_freq 0 \
     --train_and_finetune \
     --all_cols False \
     --batch_size 64 \
-    --dataset electricity \
+    --dataset multichannel \
     --epochs 50 \
     --channels 321 \
     --use_original_datahandling \
---
+++ scripts/motor/motor_720_96.sh
@@ -0,0 +1,13 @@
+python run.py \
+    --normalize True \
+    --seq_len 720 \
+    --pred_len 96 \
+    --dominance_freq 0 \
+    --train_and_finetune \
+    --all_cols False \
+    --batch_size 64 \
+    --dataset multichannel \
+    --epochs 50 \
+    --channels 321 \
+    --use_original_datahandling \
+    --H_order 10
