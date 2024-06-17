b48e8e5dd939fd412b289cbb5b6475894036fb09
Thor Hojhus <thorhojhus@gmail.com>
2024-06-17 14:38:54 +0200

    add old arima

--- src/models/ARIMA.py
+++ src/models/ARIMA.py
@@ -1,15 +1,36 @@
-from multiprocessing import Pool, cpu_count
-from torch import nn
-from statsforecast.models import AutoARIMA as auto_arima
-
 import numpy as np
 import torch
+from pmdarima.arima import auto_arima
+from threading import Thread
+from torch import nn
 
-def forecast_worker(args):
-    seq, pred_len, bt, i = args
-    model = auto_arima()
-    forecast = model.forecast(seq.numpy(), pred_len)["mean"]
-    return forecast, bt, i
+class ARIMAThread(Thread):
+    """Thread Wrapper for ARIMA."""
+    def _forecast(self):
+        if self.device == "cuda":
+            self.seq = self.seq.cpu()
+        else:
+            print(self.device)
+        model = auto_arima(self.seq)
+        forecast = model.predict(n_periods=self.pred_len)
+        return forecast, self.bt, self.i
+
+    def __init__(self, args: tuple):
+        super(ARIMAThread, self).__init__()
+        self.seq = args[0]
+        self.pred_len = args[1]
+        self.bt = args[2]
+        self.i = args[3]
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    def run(self):
+        self.result = self._forecast()
+
+    def join_result(self):
+        "Wait for and return result."
+        Thread.join(self)
+        return self.result
+    
 
 class ARIMA(nn.Module):
     """ARIMA model (auto-tuning)"""
@@ -17,25 +38,22 @@ class ARIMA(nn.Module):
     def __init__(self, configs):
         super(ARIMA, self).__init__()
         self.pred_len = configs.pred_len
-        self.device = torch.device("cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward(self, x: torch.Tensor):
         """Forward pass of ARIMA model.
         
         Will be EXTREMELY slow, but auto ARIMA is not particularly fast.
         """
-        result = np.zeros([x.shape[0], self.pred_len, x.shape[2]])
-        tasks = []
-
-        for bt, seqs in enumerate(x):
+        result = np.zeros([x.shape[0],self.pred_len,x.shape[2]])
+        arima_threads: list[ARIMAThread] = []
+        for bt,seqs in enumerate(x):
             for i in range(seqs.shape[-1]):
-                seq = seqs[:, i]
-                tasks.append((seq, self.pred_len, bt, i))
-
-        with Pool(cpu_count()) as pool:
-            results = pool.map(forecast_worker, tasks)
-        
-        for forecast, bt, i in results:
-            result[bt, :, i] = forecast
-
+                seq = seqs[:,i]
+                arima_thread = ARIMAThread(args=(seq,self.pred_len,bt,i))
+                arima_threads.append(arima_thread)
+                arima_thread.start()
+        for every_thread in arima_threads:
+            forecast,bt,i = every_thread.join_result()
+            result[bt,:,i] = forecast
         return torch.from_numpy(result).to(self.device)
