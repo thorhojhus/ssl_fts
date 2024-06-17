d49138baba4a195d1974414a90b6a699cc36dff5
Thor Hojhus <thorhojhus@gmail.com>
2024-06-17 13:09:42 +0200

    ilness

---
+++ scripts/illness/illness_48.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python run.py \
+    --normalize True \
+    --seq_len 72 \
+    --pred_len 48 \
+    --dominance_freq 0 \
+    --train_and_finetune \
+    --all_cols True \
+    --batch_size 16 \
+    --dataset national_illness \
+    --epochs 50 \
+    --use_original_datahandling \
+    --H_order 6
