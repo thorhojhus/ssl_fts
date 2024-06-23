c981ee886ff0077c18cb29c32a1a54a93da01cde
Thor Hojhus <thorhojhus@gmail.com>
2024-06-23 22:32:22 +0200

    use statsmodels instead

--- scripts/autocorrelation_plots.py
+++ scripts/autocorrelation_plots.py
@@ -2,14 +2,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from statsmodels.tsa.stattools import acf
 
 directory = '../data'
 out_dir = "../plots"
 files = os.listdir(directory)
-max_lag = 500
+max_lag = 24*10
 
-
-excluded_files = ['MRO.csv', 'gd.csv']
+excluded_files = ['MRO.csv', 'gd.csv', 'national_illness.csv']
+#files = ["national_illness.csv"]
 files = [file for file in files if file not in excluded_files and file.endswith('.csv')]
 
 def autocorrelation(x, max_lag):
@@ -26,13 +27,22 @@ for file in files:
     data = data.set_index('date')
     data = data.resample('h').mean()
     
-    plt.figure(figsize=(14,7))
+    plt.figure(figsize=(10,6))
     
-    for column in data.columns:
+    n_colors = len(data.columns)
+    colors = plt.cm.brg(np.linspace(0, 1, n_colors))
+
+    all_acf_values = []
+    for i, column in enumerate(data.columns):
         series = data[column].dropna()
-        autocorr = autocorrelation(series, max_lag)
+        autocorr = acf(series, nlags=max_lag)
         lags = np.arange(len(autocorr))    
-        plt.plot(lags, autocorr, linestyle='-', label=column)
+        plt.plot(lags, autocorr, linestyle='-', label=column)#, color=colors[i])
+        all_acf_values.append(autocorr)
+    
+    all_acf_values = np.array(all_acf_values)
+    mean_acf = np.mean(all_acf_values, axis=0)
+    plt.plot(range(len(mean_acf)), mean_acf, color='black', linewidth=3)
     
     plt.title(f'Autocorrelation Plot of {file.replace(".csv", "")}')
     plt.xlabel('Time Lag (hours)')
@@ -40,7 +50,10 @@ for file in files:
     plt.grid(True)
     plt.xlim(0, max_lag)
     
-    plot_file_path = os.path.join(directory, f'autocorr_plot_{file.replace(".csv", ".png")}')
-    plt.savefig(plot_file_path, dpi=300) 
+    xticks = np.arange(24, max_lag + 1, 24)
+    plt.xticks(xticks)
+
+    plot_file_path = os.path.join(out_dir, f'autocorr_plot_{file.replace(".csv", ".png")}')
+    plt.savefig(plot_file_path, dpi=300, bbox_inches='tight') 
     plt.close()
     print(f'Autocorrelation plot saved to {plot_file_path}')
